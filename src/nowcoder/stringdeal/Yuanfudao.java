98e38725f09c4dc278d78cbc8bdf22f47ca74c0a
n8g <niu411090502@163.com>
2020-09-13 13:23:06 +0800

    fix typo

--- src/nowcoder/stringdeal/Yuanfudao.java
+++ src/nowcoder/stringdeal/Yuanfudao.java
@@ -28,8 +28,8 @@ public class Yuanfudao {
         if (target.length() == 0) return n >= 0;
         if (n == 0) return false;
         // 求最长公共前缀的下标位置 dif1 和 dif2
-        int dif1 = firstDifferentIndexOfChar(s1, target);
-        int dif2 = firstDifferentIndexOfChar(s2, target);
+        int dif1 = firstIndexOfDifferentChar(s1, target);
+        int dif2 = firstIndexOfDifferentChar(s2, target);
         // 没有公共前缀
         if (dif1 == 0 && dif2 == 0) return false;
         // 贪心
@@ -37,7 +37,7 @@ public class Yuanfudao {
         else return dfs(s1, s2.substring(dif2), target.substring(dif2), n - 1);
     }
     
-    private int firstDifferentIndexOfChar(String a, String b) {
+    private int firstIndexOfDifferentChar(String a, String b) {
         int lena = a.length();
         int lenb = b.length();
         int len = Math.min(lena, lenb);
