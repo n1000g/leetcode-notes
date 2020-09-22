33a8955e9b4ebb99b76c71a41bd3acbf44172fdf
n8g <niu411090502@163.com>
2020-09-22 18:03:40 +0800

    fix: 300. LIS

--- src/string/dp/LIS.java
+++ src/string/dp/LIS.java
@@ -5,6 +5,37 @@ package string.dp;
  */
 public class LIS {
 
+    /**
+     * 最优解 O(NlogN)
+     * 贪心 + 二分查找
+     * 贪心思路：要使上升子序列尽可能的长，
+     *         则我们需要让序列上升得尽可能慢，
+     *         因此我们希望每次在上升子序列最后加上的那个数
+     *         尽可能的小
+     * 设计思路：维护一个数组 minVal[]
+     *         min[i] 表示长度为 i 的最长上升子序列的末尾元素的最小值
+     *         用 res 来表示最长递增子序列的长度
+     *         对数组进行迭代, 依次判断每个数 num 将其插入 minVal 数组相应的位置
+     */
+    public int lengthOfLIS2(int[] nums) {
+        int len = nums.length;
+        if (len < 2) return len;
+        int[] minVal = new int[len];
+        int res = 0;
+        for (int num : nums) {
+            int lo = 0, hi = res;
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (minVal[mid] < num) lo = mid + 1;
+                else hi = mid;
+            }
+            minVal[lo] = num;
+            if (lo == res) res++;
+        }
+        return res;
+    }
+
+    // O(N^2)
     public int lengthOfLIS(int[] nums) {
         if (nums.length < 2) return nums.length;
         int[] dp = new int[nums.length];
@@ -26,6 +57,8 @@ public class LIS {
 
     public static void main(String[] args) {
         int[] example = new int[] {-2, -1};
-        System.out.println(new LIS().lengthOfLIS(example));
+        LIS lis = new LIS();
+        System.out.println(lis.lengthOfLIS(example));
+        System.out.println(lis.lengthOfLIS2(example));
     }
 }
