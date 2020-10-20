ba7ae77fa81133de3c67c7403ed1d3b52bde90cd
n8g <niu411090502@163.com>
2020-10-20 11:28:38 +0800

    fix

--- src/arrays/topk/TopK.java
+++ src/arrays/topk/TopK.java
@@ -9,32 +9,35 @@ import java.util.*;
  *          瓶颈在排序
  *      次优：
  *          用一个优先队列维护 topK 在队列内排序
+ *          TreeMap ?
  *          O(N) + O(NlogK)
  *      最优：
  *          桶排序
  *          新建 n 个桶（频率全为 1）
- *
- *
- *
  */
 public class TopK {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 3, 4, 4, 5, 5, 5, 6, 7, 5, 7, 7, 8};
         TopK solution = new TopK();
-        System.out.println(Arrays.toString(solution.getTopK(arr, 1)));
-        System.out.println(Arrays.toString(solution.getTopK(arr, 2)));
-        System.out.println(Arrays.toString(solution.getTopK(arr, 3)));
-        System.out.println(Arrays.toString(solution.getTopK(arr, 4)));
-        System.out.println(Arrays.toString(solution.getTopK(arr, 5)));
+        System.out.println(Arrays.toString(solution.getTopK_1(arr, 1)));
+        System.out.println(Arrays.toString(solution.getTopK_1(arr, 2)));
+        System.out.println(Arrays.toString(solution.getTopK_1(arr, 3)));
+        System.out.println(Arrays.toString(solution.getTopK_1(arr, 4)));
+        System.out.println(Arrays.toString(solution.getTopK_1(arr, 5)));
     }
 
+    /**
+     * 数据量大的最优解：桶排序
+     */
     private int[] getTopK(int[] arr, int k) {
         // 空间复杂度 O(n * k)
         List<Integer>[] bucket = new List[arr.length + 1];
+        // 词频统计
         Map<Integer, Integer> frequencyMap = new HashMap<>();
         for (Integer i : arr) {
             frequencyMap.put(i, frequencyMap.getOrDefault(i, 0) + 1);
         }
+        // 为每个出现过的频率 新建一个桶并将 key 值加入这个桶里
         for (Integer key : frequencyMap.keySet()) {
             int frequency = frequencyMap.get(key);
             if (bucket[frequency] == null) {
@@ -49,5 +52,23 @@ public class TopK {
             }
         }
         return res.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    /**
+     * todo:优先队列
+     */
+    private int[] getTopK_1(int[] arr, int k) {
+        // 词频统计
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (Integer i : arr) {
+            frequencyMap.put(i, frequencyMap.getOrDefault(i, 0) + 1);
+        }
+        List<Integer> res = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k, Comparator.comparingInt(frequencyMap::get));
+        for (Integer key : frequencyMap.keySet()) {
+            if (pq.size() < k) pq.add(key);
+        }
+        System.out.println(Arrays.toString(res.toArray()));
+        return new int[0];
     }
 }
